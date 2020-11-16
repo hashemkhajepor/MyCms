6972f2ac1a781b287a3fe9a640d78c24ae0e2203
hashemkhajepour <hashemkhajepour2021@gmail.com>
2020-11-16 13:28:26 +0330

    Initial commit

---
+++ DataLayer/MetadataClasess/UsersMetaData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class UsersMetaData
+    {
+
+        public int UserID { get; set; }
+        [Display(Name = "عنوان نقش")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public int RoleID { get; set; }
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+
+        public string UserName { get; set; }
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمیباشد")]
+        public string Email { get; set; }
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "کد فعال سازی")]
+        public string ActiveCode { get; set; }
+        [Display(Name = "وضعیت")]
+
+        public bool IsActive { get; set; }
+        [Display(Name = "تاریخ ثبت نام")]
+
+        public System.DateTime RegisterDate { get; set; }
+
+        public virtual Roles Roles { get; set; }
+    }
+    [MetadataType(typeof(UsersMetaData))]
+    public partial class Users
+    {
+
+    }
+}
